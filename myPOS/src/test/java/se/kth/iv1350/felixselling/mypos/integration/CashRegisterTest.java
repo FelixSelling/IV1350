d12d3f4a41e1bc0c3154a69064ceb4f0da299880
FelixSelling <fselling@kth.se>
2022-06-05 14:21:14 +0200

    Improved testing

--- myPOS/src/test/java/se/kth/iv1350/felixselling/mypos/integration/CashRegisterTest.java
+++ myPOS/src/test/java/se/kth/iv1350/felixselling/mypos/integration/CashRegisterTest.java
@@ -7,18 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CashRegisterTest {
 
+    private CashRegister cashRegisterWithInitial5000;
+
+    @BeforeEach
+    public void setUp() {
+        cashRegisterWithInitial5000 = new CashRegister(5000);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        cashRegisterWithInitial5000 = null;
+    }
+
     @Test
     public void testAddCash() {
-        double cash = 5000;
-        CashRegister instance = new CashRegister();
-        instance.addCash(cash);
+        double expected = 5000 + 1000;
+        cashRegisterWithInitial5000.addCash(1000);
+        double actual = cashRegisterWithInitial5000.getCash();
+        assertEquals(expected, actual, "CashRegisters cash balance and expected cash balance not equal.");
     }
 
     @Test
     public void testRemoveCash() {
-        double cash = 5000;
-        CashRegister instance = new CashRegister();
-        instance.removeCash(cash);
+
+        double expected = 5000 - 1000;
+        cashRegisterWithInitial5000.removeCash(1000);
+        double actual = cashRegisterWithInitial5000.getCash();
+        assertEquals(expected, actual, "CashRegisters cash balance and expected cash balance not equal.");
     }
-    
 }
